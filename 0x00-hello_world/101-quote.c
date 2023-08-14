5d4e618eaf5d0b7d09f8b4f4611ca5fcd76995c5
Mundez-the-man <vicmundez@gmail.com>
2023-08-14 09:48:00 -0700

    8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,9 @@
 #include <unistd.h>
-#include <string.h>
 
-/**
- * main - uses the write system call to write the contents to the standard error stream
- * Return: Always 1
- */
-int main(int argc, char *argv[])
+int main(void)
 {
-	char buf[]= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(2, buf, strnlen(buf, sizeof(buf)));
+	const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, sizeof(message) - 1);
 
-	return (1);
+	return 1;
 }
