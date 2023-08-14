045593e9868d9449e370ce12d5b20cb6019f79b7
Mundez-the-man <vicmundez@gmail.com>
2023-08-14 03:37:06 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-/*main function to print in stdout*/
+/**
+ * main - function to print in stdout
+ * Return: Always 0.
+ */
 int main(void)
 {
 
 	/*String initialization*/
-	char prog[] = ""Programming is like building a multilingual puzzle\n";
+	char prog[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(prog);
 
