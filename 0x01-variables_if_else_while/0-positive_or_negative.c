9f61b03e06746e28f1adf71589ff417796bbdc95
Mundez-the-man <vicmundez@gmail.com>
2023-08-15 06:27:50 -0700

    0. Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,22 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <time.h>
+#include <stdio.h>
+/* more headers goes there */
 
 /**
- * main - Positive or Negative - print sign of random number
- * @void: Empty parameter list for main.
- *
- * Description: this if/else statement delares the sign (positive
- * or negative) of a random number. It also states if the value
- * is zero.
- *
- * Return: 0 for success
-*/
+ * main - to check if number is positive, negative, or zero
+ * Return: always 0
+ */
 int main(void)
 {
-  /**
-   * variable n has been declared
-   * coding then assigns the random value.
-   */
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-	if (n > 0)
+	/* if else statement to check n */
+	if (n < 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is negative\n", n);
 	}
 	else if (n == 0)
 	{
@@ -33,8 +24,7 @@ int main(void)
 	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is positive\n", n);
 	}
-
 	return (0);
 }
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,4 +1,4 @@
-0. Positive anything is better than negative nothing
+0. print whether the number stored in the variable n is positive or negative
 1. The last digit
 2. a program that prints the alphabet in lowercase, followed by a new line
 3. a program that prints the alphabet in lowercase, and then in uppercase
