986c4eb4223d9192a39896ed06196eeefcd3e21f
Mundez-the-man <vicmundez@gmail.com>
2023-08-20 14:56:28 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,30 +1,42 @@
 #include "main.h"
 
+/**
+ * times_table - a function that prints the 9 times table
+ */
 void times_table(void)
 {
+	/*variables declaration*/
 	int i, j, mul;
+
+	/*for loops to produce the times table of 9*/
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 10; j++)
 		{
 			mul = i*j;
+			
+			/* if statement for first space after the comma*/
 			if (j !=0 )
 			{
 				_putchar(' ');
-			}
-			if (mul < 10)
-			{
-				if(j != 0)
+				
+				/*if statement for second space after ',' for multiples less than 9*/
+				if (mul < 10)
 				{
 					_putchar(' ');
 				}
-				_putchar('0' + mul);
 			}
-			else
+			
+			/*if statement for the digit in the tens position*/
+			if (mul >= 10)
 			{
 				_putchar('0' + mul / 10);
-				_putchar('0' + mul % 10);
 			}
+			
+			/*to get the digits in the ones position*/
+			_putchar('0' + mul % 10);
+
+			/*if statement to print ',' after all multiples except for multiples of 9*/
 			if (j != 9)
 			{
 				_putchar(',');
--- 0x02-functions_nested_loops/gpt-9-times_table.c
+++ 0x02-functions_nested_loops/gpt-9-times_table.c
@@ -1,31 +1,29 @@
 #include "main.h"
 
-void times_table(void)
-{
-    int i, j, mul;
+void times_table(void) {
+   	int i, j, mul;
 
-    for (i = 0; i < 10; i++)
-    {
-        for (j = 0; j < 10; j++)
-        {
+       	for (i = 0; i < 10; i++) {
+        for (j = 0; j < 10; j++) {
             mul = i * j;
 
-            if (j > 0)
-            {
-                _putchar(',');
+            if (j != 0) {
                 _putchar(' ');
             }
 
-            if (mul < 10)
+            if (mul < 10) {
                 _putchar(' ');
-            else
+            } else {
                 _putchar('0' + mul / 10);
+            }
 
             _putchar('0' + mul % 10);
 
-            if (j == 9)
-                _putchar('\n');
+            if (j != 9) {
+                _putchar(',');
+            }
         }
+        _putchar('\n');
     }
 }
 
