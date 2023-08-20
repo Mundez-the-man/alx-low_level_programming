9eb6a6f549c4ef9b21652d829f9de873a4718ed7
Mundez-the-man <vicmundez@gmail.com>
2023-08-20 16:54:33 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,29 +13,24 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			mul = i*j;
-			
+			mul = i * j;
 			/* if statement for first space after the comma*/
-			if (j !=0 )
+			if (j != 0)
 			{
 				_putchar(' ');
-				
 				/*if statement for second space after ',' for multiples less than 9*/
 				if (mul < 10)
 				{
 					_putchar(' ');
 				}
 			}
-			
 			/*if statement for the digit in the tens position*/
 			if (mul >= 10)
 			{
 				_putchar('0' + mul / 10);
 			}
-			
 			/*to get the digits in the ones position*/
 			_putchar('0' + mul % 10);
-
 			/*if statement to print ',' after all multiples except for multiples of 9*/
 			if (j != 9)
 			{
