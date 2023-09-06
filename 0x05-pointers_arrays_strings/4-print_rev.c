18e624e42902e8896b23d07087a41e690105f04e
Mundez-the-man <vicmundez@gmail.com>
2023-09-06 07:38:27 -0700

    problem 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- * print_rev - prints a string, in reverse.
- * @s: string to be inputted.
- * Return: nothing.
+ * print_rev - prints a string in reverse
+ * @s: string to be printed in reverse
  */
+
 void print_rev(char *s)
 {
-	int count = 0;
-
+	/* Initialize variable to count characters */
+	int count;
+	/* loop to count length of string */
+	for (count = 0; s[count] != '\0'; count++)
+	{
+	}
+	/* print the characters of a string in reverse */
 	while (count >= 0)
 	{
-		if (s[count] == '\0')
-			break;
-		count++;
-	}
-
-	for (count--; count >= 0; count--)
 		_putchar(s[count]);
+		count--;
+	}
+	/*print new liine after the string */
 	_putchar('\n');
+
 }
