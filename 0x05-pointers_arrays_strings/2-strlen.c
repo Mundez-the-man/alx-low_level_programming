6f51c0d890f6450af58bf7cf6577ae550b6ed5bb
Mundez-the-man <vicmundez@gmail.com>
2023-09-06 03:20:42 -0700

    problem 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,15 +1,20 @@
 #include "main.h"
 
 /**
- * _strlen - returns the length of a string.
- * @s: string to be inputted.
- * Return: string length.
+ * _strlen - returns the length of a string
+ * @s: String to be checked
+ *
+ * Return: length of string (i)
  */
+
 int _strlen(char *s)
 {
-	int count = 0;
-
-	while (*(s + count) != '\0')
-		count++;
-	return (count);
+	/* Declare an integer variable for counting characters */
+	int i;
+	/* Loop through the string until the null character ('\0') is encountered */
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	/* return the length of string */
+	return (i);
 }
