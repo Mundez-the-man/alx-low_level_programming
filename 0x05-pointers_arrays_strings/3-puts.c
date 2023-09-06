a4c3e3eaee9d7d9b8d57d8b38d28c77d315e37db
Mundez-the-man <vicmundez@gmail.com>
2023-09-06 04:14:29 -0700

    problem 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: string to be printed
+ */
+
+void _puts(char *str)
+{
+	/* Initialize variable to count characters */
+	int count;
+	/* loop through until a null character (\0) is encountered */
+	for (count = 0; str[count] != '\0'; count++)
+	{
+		_putchar(str[count]);
+	}
+	/*print new liine after the string */
+	_putchar('\n');
+
+}
