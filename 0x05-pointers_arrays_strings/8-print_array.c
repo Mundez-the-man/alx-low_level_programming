3f2df155fadd3146676c711c5eddfed433b47da5
Mundez-the-man <vicmundez@gmail.com>
2023-09-11 14:57:50 -0700

    problem 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,16 +12,16 @@ void print_array(int *a, int n)
 	/* Initialize variable to count characters */
 	int num;
 
-	if (n > 0)
+	if (n > 0) /* Ignore the case where n is 0 or negative */
 	{
-		printf("%d", a[0]);
-		/* print the elements of the array */
+		printf("%d", a[0]); /* Print the first element without a leading comma */
+		/* print the remaining elements with a leading comma and space. */
 		for (num = 1; num < n; num++)
 		{
 			printf(", %d", a[num]);
 		}
 	}
-	/* print new line after the string */
+	/* print new line after the string or if n is 0 or negative */
 	putchar('\n');
 
 }
