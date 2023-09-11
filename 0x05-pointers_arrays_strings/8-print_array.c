541cf1c4dca7684191fdab8bbed45ce61ee9d8a6
Mundez-the-man <vicmundez@gmail.com>
2023-09-11 14:42:47 -0700

    problem 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,15 +11,12 @@ void print_array(int *a, int n)
 {
 	/* Initialize variable to count characters */
 	int num;
-	/* loop through until a null character (\0) is encountered */
-	for (num = 0; a[num] < a[n]; num++)
-	{
-		printf("%d", *(a + num));
 
-		if (a[num] != a[n - 1])
-		{
-			printf(", ");
-		}
+	printf("%d", a[0]);
+	/* print the elements of the array */
+	for (num = 1; num < n; num++)
+	{
+		printf(", %d", a[num]);
 	}
 	/*print new line after the string */
 	putchar('\n');
