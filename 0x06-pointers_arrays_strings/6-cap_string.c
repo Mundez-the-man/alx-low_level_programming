db1f1ce29790775a687f2c367ff83f7a2f2e2f19
Mundez-the-man <vicmundez@gmail.com>
2023-11-02 07:46:56 -0700

    problem 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,32 +1,30 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters of a string to uppercase
- * @a: string
+ * cap_string - capitalizes all words of a string after word separators
+ * @a: string to be capitalized
  *
  * Return: pointer to the converted string
  */
 char *cap_string(char *a)
 {
-	int i;
+	/* Initialize counters */
+	int i, n;
+	/* Intilize word separataors in ASCII */
+	int cap[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
-		if (a[i] == 32 || a[i] == 9 || a[i] == 10 || a[i] == 44)
-			if (a[i + 1] > 96 && a[i + 1]  < 123) /* check if character is lowercase */
+		for (n = 0; cap[n] != '\0'; n++)
+		{
+			if (a[i] == cap[n])
 			{
-				a[i + 1] -= 32; /* change lower case character to uppercase */
+				if (a[i + 1] > 96 && a[i + 1]  < 123)
+				{
+					a[i + 1] -= 32; /* change lower case character to uppercase */
+				}
 			}
-		if ( a[i] == 59 || a[i] == 46 || a[i] == 33 || a[i] == 63 )
-			if (a[i + 1] > 96 && a[i + 1]  < 123) /* check if character is lowercase */
-			{
-				a[i + 1] -= 32; /* change lower case character to uppercase */
-			}
-		if ( a[i] == 34 || a[i] == 40 || a[i] == 41 || a[i] == 123 || a[i] == 125)
-			if (a[i + 1] > 96 && a[i + 1]  < 123) /* check if character is lowercase */
-			{
-				a[i + 1] -= 32; /* change lower case character to uppercase */
-			}
+		}
 	}
 
 	return (a);
