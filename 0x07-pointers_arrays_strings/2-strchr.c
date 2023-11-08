a0dd4ad2945fdc0d781cedbaf8fe9b88d7fc71a5
Mundez-the-man <vicmundez@gmail.com>
2023-11-08 05:20:00 -0800

    problem 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,9 +8,13 @@
  *
  * Return: pointer to first occurence of c, or NULL if not found
  */
-
 char *_strchr(char *s, char c)
 {
+	if (*s == c)
+	{
+		return (s);
+	}
+
 	/* Loop through the characters in the string */
 	for (; *s != '\0'; s++)
 	{
