f0e6546be5384777e885e739206368211dbc11ae
Mundez-the-man <vicmundez@gmail.com>
2023-11-09 13:52:08 -0800

    problem 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stddef.h>
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, k, j;
+	char *location;
+
+	i = 0;
+	j = 0;
+
+	while (haystack[i] != '\0')
+	{
+		while (needle[j] != '\0')
+		{
+			if (haystack[i] == needle[j])
+			{
+				j++;
+				break;
+			}
+			if (haystack[i] != needle[j])
+			{
+				j = 0;
+				break;
+			}
+		}
+		if (needle[j] == '\0')
+		{
+			k = i - (j - 1);
+			location = &haystack[k];
+			return (location);
+		}
+		i++;
+	}
+	return (NULL);
+}
